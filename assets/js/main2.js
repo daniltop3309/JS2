a7a5df67bced4eea60a81bbd38580c1c5b71a8c5
daniltop3309 <114923511+daniltop3309@users.noreply.github.com>
2022-10-24 15:25:06 +0300

    Add files via upload

---
+++ assets/js/main2.js
@@ -0,0 +1,248 @@
+
+console.log("if, swith");
+
+// let a = 10;
+
+// if (a ==10){
+//     console.log("Верно")
+// }
+// else{
+//     console.log('Неверно')
+// }
+
+
+// let min = 35;
+
+// let fourth = (min < 15 && min > 0)? 'Первая четверть' :
+//     (min > 14 && min < 30)? 'Вторая четверть':
+//     (min > 29 && min < 45)? 'Третья четверть':
+//     (min > 44 && min < 60)? 'Четвертая четверть':
+
+// alert(fourth);
+
+
+
+// let lang = 'ru';
+// let arr = {
+//     'ru': ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вск'],
+//     'en': ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+// }
+
+// console.log(arr[lang])
+
+
+
+console.log("if, else");
+
+// let a = 0;
+
+// if (a == 0){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = 1;
+
+// if (a > 0){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = -1;
+
+// if (a < 0){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = 0;
+
+// if (a >= 0){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = 0;
+
+// if (a <= 0){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = 0;
+
+// if (a = 0){
+//     alert('Неверно')
+// }
+// else{
+//     alert("Верно")
+// }
+
+
+
+// let a = 'test';
+
+// if (a == 'test'){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = '1';
+
+// if (a == '1'){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+console.log("Работа с && (и) и || (или)");
+
+// let a = 1;
+
+// if (a > 0 && a < 5){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+
+// let a = 2;
+
+// if (a == 0 || a == 2){
+//     alert(a + 7)
+// }
+// else{
+//     alert(a / 10)
+// }
+
+
+
+// let a = 1;
+// let b = 3;
+
+// if (a <= 1 || b >= 3){
+//     alert(a + b)
+// }
+// else{
+//     alert(a - b)
+// }
+
+
+
+// let a = 2;
+// let b = 3;
+
+// if (a > 2 && a < 11 || b >= 6 && b < 14){
+//     alert('Верно')
+// }
+// else{
+//     alert("Неверно")
+// }
+
+
+console.log("Задачи на switch-case");
+
+// let num = 3;
+// var result
+
+// switch (num) {
+//     case 3:
+//       alert(result = 'лето');
+//       break;
+//     case 4:
+//       alert(result = 'осень');
+//       break;
+//     case 1:
+//       alert(result = 'зима');
+//       break;
+//     case 2:
+//       alert(result = 'весна');
+//   }
+
+console.log("Задачi");
+
+// let day = 15;
+
+// let three = (day > 0 && day < 11)? 'Первая декада' :
+//     (day > 10 && day < 21)? 'Вторая декада':
+//     (day > 21 && day < 32)? 'Третья декада':
+
+// alert(three);
+
+
+
+// let month = 1;
+
+// let god = (month == 12 && month == 1 && month == 2)? 'Зима' :
+//     (month > 2 && month < 6)? 'Весна':
+//     (month > 5 && month < 9)? 'Лето':
+//     (month > 8 && month < 12)? 'Осень':
+
+// alert(god);
+
+
+
+// let str = 'abcde';
+
+// if (str[0] == 'a'){
+//     alert('да')
+// }
+// else{
+//     alert("нет")
+// }
+
+
+// let str = '12345';
+
+// if (str[0] == '1' || str[0] == '2' || str[0] == '3'){
+//     alert('да')
+// }
+// else{
+//     alert("нет")
+// }
+
+
+// let str = '123';
+
+// alert(Number(str[0]) + Number(str[1]) + Number(str[2]))
+
+
+
+// let str = '123321';
+// one = Number(str[0]) + Number(str[1]) + Number(str[2])
+// two = Number(str[3]) + Number(str[4]) + Number(str[5])
+
+// if (one == two){
+//     alert('да')
+// }
+// else{
+//     alert('нет')
+// }
